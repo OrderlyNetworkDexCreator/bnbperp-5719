2cc82783c4f920f99743124191454744275e6178
Mario Reder <mario.reder@pm.me>
2026-01-22 15:33:38 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/OrionXBSC",
   "VITE_SEO_SITE_NAME": "OrionX",
   "VITE_SEO_SITE_DESCRIPTION": "No.1 Trading Ecosystem on BNB Smart Chain",
-  "VITE_SEO_SITE_URL": "https://bnbperp.trade",
+  "VITE_SEO_SITE_URL": "https://orionx.trade",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@OrionXBSC",
